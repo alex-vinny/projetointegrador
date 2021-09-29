10652be74ee626872400d8d6167fd47a48f15e1d
cpiresjr <carllos.pires@yahoo.com.br>
2021-09-29 17:34:46 -0300

    Desenvolvendo

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      LoginComponent,
+      CadastroComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
