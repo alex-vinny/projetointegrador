a55f6089a8da234ee70d7ee9bc6b4f167109a9a7
Alex Vinicius Zâmboli <alex.zamboli@gmail.com>
2021-10-14 22:00:03 -0300

    Correção na consulta de usuários. Hierarquia das classes.

---
+++ webapi/Models/Administrador.cs
@@ -0,0 +1,11 @@
+using ProjetoIntegrador.Api.Dtos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoIntegrador.Api.Models
+{
+    public class Administrador : Usuario
+    {        
+    }
+}
