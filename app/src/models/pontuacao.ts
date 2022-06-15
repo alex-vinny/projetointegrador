70b642ba9e584b395a5d424557263a6ea416243f
cpiresjr <carllos.pires@yahoo.com.br>
2022-06-15 20:31:43 -0300

    chore: Criando interface pontuação

--- app/src/models/pontuacao.ts
+++ app/src/models/pontuacao.ts
@@ -1,2 +1,10 @@
 export interface Pontuacao {
+   qtdItems: number;
+   qtdPontos: number;
+   qtdErros: number;
+   tipoJogoId: number;
+   tipoJogo: string;
+   email: string;
+   usuario: number;
+   dataJogo: Date;
 }
